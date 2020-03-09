d3269f4dd3292fb8bd65e26e270b7ab68c55f81c
williamtsease <williamtsease@gmail.com>
2020-03-09 11:16:08 -0700

    Customized random snake

--- app/server.py
+++ app/server.py
@@ -28,7 +28,7 @@ def start():
     data = bottle.request.json
     print("START:", json.dumps(data))
 
-    response = {"color": "#00FF00", "headType": "regular", "tailType": "regular"}
+    response = {"color": "#2b8020", "headType": "fang", "tailType": "round-bum"}
     return HTTPResponse(
         status=200,
         headers={"Content-Type": "application/json"},
