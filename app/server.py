8b7cf5421f71ee647e8c74165ccffc08839af1c6
williamtsease <williamtsease@gmail.com>
2020-03-11 12:49:39 -0700

    everything

--- app/server.py
+++ app/server.py
@@ -80,12 +80,11 @@ def interpretBoard(boardInfo):
     board = [[-1 for i in range(boardInfo['height'])] for j in range(boardInfo['width'])] 
     for food in boardInfo['food']:
         board[food['x']][food['y']] = 1
- #   for snake in boardInfo['snakes']:
- #       snakeln = len(snake['body'])
- #       for segment in snake['body']:
- #           board[segment['x']][segment['y']] = 100 + snakeln
- #           snakeln -= 1
- #   board[headx][heady] += 100
+    for snake in boardInfo['snakes']:
+        snakeln = len(snake['body'])
+        for segment in snake['body']:
+            board[segment['x']][segment['y']] = 100 + snakeln
+            snakeln -= 1
     return board
 
 @bottle.post("/end")
